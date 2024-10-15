06f03f0b9ac7fad1ddd30827bc417ad852b31db1
Anthony <anthonychinaemeru84@yahoo.com>
2024-10-15 22:49:38 +0100

    Signup view authorization check

---
+++ dashboard/tasks.md
@@ -0,0 +1,6 @@
+Working on the Signup view
+1. Check if the email is already existing
+2. Check if the passwords match
+3. Check if the user password is more than 8 characters
+
+if users are ok, create_user()
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.utils import timezone
+from django.urls import reverse
+from .models import *
+
 
 # Create your views here.
 # @login_required
@@ -20,4 +29,30 @@ def signup_view(request):
         password2 = request.POST.get('password2')
 
         user_details_have_error = False
+
+        # Check if email already exists
+        if User.objects.filter(email=email).exists():
+            user_details_have_error = True
+            messages.error(request, "user with this email already exists")
+
+        # Check if the passwords match
+        if password != password2:
+            user_details_have_error = True
+            messages.error(request, "password doesn't match")
+
+        if len(password) < 8:
+            user_details_have_error = True
+            messages.error(request, "password must not be less than 8 characters")
+        
+        else:
+            new_user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                gender=gender,
+                password=password
+            )
+            messages.success(request, "Account created successfully")
+            return redirect('loginUser')
+
     return render(request, 'dashboard/signup.html')
